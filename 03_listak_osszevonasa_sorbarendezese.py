f14bb98ff759d4dada0c9968d10904773df46c37
GondiZsolt <gondi.zsolt@diak.szbi-pg.hu>
2024-11-20 14:26:29 +0100

    add

--- 03_listak_osszevonasa_sorbarendezese.py
+++ 03_listak_osszevonasa_sorbarendezese.py
@@ -2,6 +2,17 @@
 Hozz létre két listát: [3, 1, 4] és [9, 7, 2]. 
 Egyesítsd a két listát, és rendezd a lista elemeit növekvő sorrendbe!
 """
+szamok1 = [3, 1, 4]
+szamok2 = [9, 7, 2]
 
+szamok1.extend(szamok2)
+szamok1.sort()
+print(szamok1)
 
 # írasd ki az első és az utolsó elemet!
+szamok1 = [3, 1, 4]
+szamok2 = [9, 7, 2]
+
+szamok1.extend(szamok2)
+szamok1.sort()
+print(szamok1)
